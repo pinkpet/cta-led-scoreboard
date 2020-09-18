d4f171480952313fcebcfa22d72053d326a89435
Bob Leavitt <bobleavitt@gmail.com>
2020-09-17 22:17:30 -0500

    rect ii

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -108,7 +108,7 @@ class CtaTrainTracker:
                 logo_renderer.render()
 
                 self.matrix.draw.rectangle([0, 20, 26, 10], fill=(255, 0, 0))
-                self.matrix.draw.rectangle((0, 0, 31, 31), fill=(0, 255, 0), outline=(0, 0, 255))
+                self.matrix.draw.rectangle((0, 25, 64, 8), fill=(0, 0, 200), outline=(0, 0, 255))
                 self.matrix.render()
                 self.sleepEvent.wait(5)
                 self.matrix.draw_image_layout(
