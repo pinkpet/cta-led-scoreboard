9c282790971cfaa0eb8b56d64a36210b25a4553e
Bob Leavitt <bobleavitt@gmail.com>
2020-09-17 22:42:36 -0500

    rect x

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -128,7 +128,7 @@ class CtaTrainTracker:
                 if self.data.newUpdate and not self.data.config.clock_hide_indicators:
                     self.matrix.update_indicator()
 
-            self.matrix.draw.rectangle([5, 5, 26, 10], fill=(255, 0, 0))
+            self.matrix.draw.rectangle([0, 50, 64, 8], fill=(255, 0, 0))
             self.matrix.render()
 
             self.sleepEvent.wait(5)
