95faef4501250a5ac7d2a31665586415828269f5
Bob Leavitt <bobleavitt@gmail.com>
2020-09-19 22:54:10 -0500

    bottom bar stuff ii

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -144,7 +144,7 @@ class CtaTrainTracker:
                 self.matrix.draw_image_layout(
                 self.layout.info,
                 image_bottom_bar,
-                (0,25)
+                (0,0)
                 )
 
                 self.matrix.draw_image_layout(
