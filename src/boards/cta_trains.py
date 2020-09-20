2c4904a977c6a67dd5f8a06f7cf2299eaa07e9d6
Bob Leavitt <bobleavitt@gmail.com>
2020-09-20 14:48:34 -0500

    see if we can get this image going ii-updated file name, added get_file command.

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -172,7 +172,7 @@ class CtaTrainTracker:
         image = Image.new('RGB', (64, train_scroller_height))
         draw = ImageDraw.Draw(image)
         image = Image.open(get_file('assets/images/cta-logo.ppm').convert('RGB')
-        image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
+#        image.resize((self.matrix.width, self.matrix.height), Image.ANTIALIAS)
 
         double_buffer = self.matrix.CreateFrameCanvas()
         img_width, img_height = self.image.size
