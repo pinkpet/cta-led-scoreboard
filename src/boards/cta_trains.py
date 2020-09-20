529f11030581680b65f70fea43d4a5df93195140
Bob Leavitt <bobleavitt@gmail.com>
2020-09-19 23:03:12 -0500

    bottom bar stuff iv

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -110,7 +110,6 @@ class CtaTrainTracker:
 
                 logo_renderer.render()
 
-
                 self.matrix.draw_image_layout(
                     self.layout.info,
                     image,
@@ -194,7 +193,7 @@ class CtaTrainTracker:
     def draw_bottom_bar(self):
         image_bottom_bar = Image.new('RGB', (64,8))
         draw = ImageDraw.Draw(image_bottom_bar)
-        self.matrix.draw.rectangle([0, 0, self.matrix.width, 8], fill=(0, 0, 205))
+        self.matrix.draw.rectangle([0, 0, self.matrix.width, 8], fill=(250, 0, 0))
 
         # self.matrix.draw.rectangle([0, 23, self.matrix.width, self.matrix.height], fill=(0, 0, 205))
 
