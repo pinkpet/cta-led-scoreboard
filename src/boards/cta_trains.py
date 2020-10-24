3e9049614316a53a570e6e6426b4a2b4bed82cf9
Bob Leavitt <bobleavitt@gmail.com>
2020-10-24 18:42:12 -0500

    adjusting bottom bar

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -76,18 +76,6 @@ class CtaTrainTracker:
                 'team_summary'
             )
 
-            # try:
-            #     if prev_game:
-            #         prev_game_id = self.teams_info[team_id].previous_game.dates[0]["games"][0]["gamePk"]
-            #         prev_game_scoreboard = Scoreboard(nhl_api.overview(prev_game_id), self.data)
-            #     else:
-            #         prev_game_scoreboard = False
-            #
-            #     self.data.network_issues = False
-            # except ValueError:
-            #     prev_game_scoreboard = False
-            #     self.data.network_issues = True
-
             stats = team.stats
             im_height = 64
             team_abbrev = team.abbreviation
@@ -117,7 +105,7 @@ class CtaTrainTracker:
                 self.matrix.draw_image_layout(
                     self.layout.info,
                     image_bottom_bar,
-                    (0, 20)
+                    (0, 22)
                 )
 
                 # self.matrix.draw.rectangle([0, 0, self.matrix.width, 8], fill=(250, 0, 0))
@@ -150,7 +138,7 @@ class CtaTrainTracker:
                 self.matrix.draw_image_layout(
                 self.layout.info,
                 image_bottom_bar,
-                (0, 20)
+                (0, 22)
                 )
 
                 self.matrix.render()
