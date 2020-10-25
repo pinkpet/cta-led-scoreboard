0dab7edf3828867899fc992f4d15c29ae7e5600a
Bob Leavitt <bobleavitt@gmail.com>
2020-10-24 20:54:31 -0500

    doing the thing where only the bottom train scrolls

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -84,11 +84,11 @@ class CtaTrainTracker:
                     2
                 )
 
-                image_train_bottom_rest = self.draw_train_times(
-                    trains,
-                    2,
-                    len(trains)
-                )
+                # image_train_bottom_rest = self.draw_train_times(
+                #     trains,
+                #     2,
+                #     len(trains)
+                # )
 
                 #cta SCROLLER!
                 cta_logo_image = Image.open(get_file('assets/images/quad-ctas.png'))
