9d01e42e75ff34ccc1c6a1e5168f9c7980209420
Bob Leavitt <bobleavitt@gmail.com>
2020-10-24 20:37:26 -0500

    doing the thing where only the bottom train scrolls

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -155,12 +155,12 @@ class CtaTrainTracker:
         pos = 0
         train_max = 2
         train_count = 0
+        print(cta_data.dtype)
         while train_count < train_max:
             train_count += 1
-            for train in cta_data:
-                draw.text((1, pos), "{}".format(train['Dest']), fill=(255, 255, 255), font=self.font, align="right")
-                draw.text((40,pos), "{}".format(train['Time']), fill=(255, 255, 255), font=self.font, align="center")
-                pos += 7
+            draw.text((1, pos), "{}".format(cta_data[train_count]['Dest']), fill=(255, 255, 255), font=self.font, align="right")
+            draw.text((40,pos), "{}".format(cta_data[train_count]['Time']), fill=(255, 255, 255), font=self.font, align="center")
+            pos += 7
 
         print(cta_data)
 
