5cc271dd965cb66709b778663b69df85253a1113
Bob Leavitt <bobleavitt@gmail.com>
2020-10-24 19:32:20 -0500

    implementing logo scroller

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -85,7 +85,7 @@ class CtaTrainTracker:
 
                 cta_logo_image = Image.open(get_file('assets/images/quad-ctas.png'))
                 cta_img_width, cta_img_height = cta_logo_image.size
-
+                print(cta_img_width + "IS THE WIDTH")
                 cta_xpos = 0
                 while cta_xpos < cta_img_width:
                     self.matrix.clear()
