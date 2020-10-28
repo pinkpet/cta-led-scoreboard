674fcb1e93cd956fca59af2d05a343772458a49b
Bob Leavitt <bobleavitt@gmail.com>
2020-10-27 22:33:45 -0500

    fun images

--- src/boards/cta_trains.py
+++ src/boards/cta_trains.py
@@ -137,6 +137,10 @@ class CtaTrainTracker:
                 cta_logo_image = Image.open(get_file('assets/images/cta/cta-' + str(png_number) + '.png'))
                 cta_img_width, cta_img_height = cta_logo_image.size
                 cta_xpos = 0
+                self.matrix.clear()
+                self.matrix.draw_image((-cta_xpos, 0), cta_logo_image, "top-left")
+                self.matrix.render()
+                self.sleepEvent.wait(2)
                 while cta_xpos < cta_img_width - 64:
                     cta_xpos += 1
                     self.matrix.clear()
